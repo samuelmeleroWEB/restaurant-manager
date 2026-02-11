f4826759cd34b68dbb4ec41ce86294dd30669c5c
Samuel <samuelmelero98@gmail.com>
2026-02-11 19:53:52 +0100

    feat: implementar login con generación de JWT

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 import User from "../models/user.js";
-
+import jwt from 'jsonwebtoken';
 
 export const register = async (req, res) => {
     try {
@@ -24,5 +24,43 @@ export const register = async (req, res) => {
 
     } catch (error) {
         res.status(500).json({ message: "Error en el servidor", error: error.message });
+    }
+};
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        // 1. ¿Existe el usuario?
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).json({ message: "Credenciales inválidas" }); // No decimos cuál para no dar pistas a hackers
+        }
+
+        // 2. ¿La contraseña coincide? (Usamos el método del modelo)
+        const isMatch = await user.verifyPassword(password);
+        if (!isMatch) {
+            return res.status(401).json({ message: "Credenciales inválidas" });
+        }
+
+        // 3. Crear el Token
+        const token = jwt.sign(
+            { id: user._id, role: user.role },
+            process.env.JWT_SECRET || 'clave_secreta_provisional',
+            { expiresIn: '8h' } // El ticket dura 8 horas
+        );
+
+        // 4. Responder (Sin la contraseña)
+        res.json({
+            message: "Login exitoso",
+            token,
+            user: {
+                email: user.email,
+                role: user.role
+            }
+        });
+
+    } catch (error) {
+        res.status(500).json({ message: "Error en el login", error: error.message });
     }
 };
