183d2b737d310757b8cf8e406bf38b2f1d3c73ca
Samuel <samuelmelero98@gmail.com>
2026-02-23 19:31:26 +0100

    feat: endpoint para listar usuarios y configuración de CORS

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-import { register, login } from '../controllers/auth.controller.js';
+import { register, login, getUsers } from '../controllers/auth.controller.js';
 
 const router = Router();
 
 router.post('/register', register);
 router.post('/login', login);
-
+router.get('/users', getUsers); // Ruta para ver todos
 
 
 export default router;
